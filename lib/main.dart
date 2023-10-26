a46d64e3f2fa1e12744eb4a074c555eb7fead2b8
=Ziedny28 <=ziednymubarok1117@gmail.com>
2023-10-26 15:43:49 +0700

    fixed midpage-start from homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tap_scan/pages/pdf_page.dart';
+import 'package:tap_scan/pages/welcome_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: "Poppins",
       ),
-      home: const PdfPage(),
+      home: const WelcomePage(),
     );
   }
 }
